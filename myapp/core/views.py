4decad31d610d3c43b3946496d231467ac417bca
Mikhail <mishanyaveselyi@gmail.com>
2022-04-08 15:58:17 -0400

    Add 2 html files and set view for them

--- myapp/core/views.py
+++ myapp/core/views.py
@@ -1,13 +1,13 @@
-# core/views.py 
-
 from flask import render_template, request, Blueprint
+from myapp.models import JokePost
 
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    
-    return render_template('index.html')
+    page = request.args.get('page', 1, type=int)
+    joke_posts = JokePost.query.order_by(JokePost.date.desc()).paginate(page=page, per_page=5)
+    return render_template('index.html', joke_posts=joke_posts)
 
 @core.route('/info')
 def info():
