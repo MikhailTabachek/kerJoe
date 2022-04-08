bc68dd0885433b0d280c83a9076e28c68c2b1b9f
Mikhail <mishanyaveselyi@gmail.com>
2022-04-08 15:43:51 -0400

    Set Joke create Route and controller

--- myapp/joke_posts/views.py
+++ myapp/joke_posts/views.py
@@ -4,4 +4,17 @@ from myapp import db
 from myapp.models import JokePost
 from myapp.joke_posts.forms import JokePostForm
 
-blog_posts = Blueprint('joke_posts', __name__)
+joke_posts = Blueprint('joke_posts', __name__)
+
+@joke_posts.route('/create', methods=['GET', 'POST'])
+@login_required
+def create_post():
+    form = JokePostForm()
+    if form.validate_on_submit():
+        joke_post = JokePost(title=form.title.data, text=form.text.data, user_id=current_user.id)
+        db.session.add(joke_post)
+        db.session.commit()
+        flash('Joke Created')
+        print('Joke was created')
+        return redirect(url_for('core.index'))
+    return render_template('create_post.html', form=form)
