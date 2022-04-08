210f892ced54cab3c559b41fd33cf4c4df1c3391
Mikhail <mishanyaveselyi@gmail.com>
2022-04-08 15:48:21 -0400

    Set Edit joke route

--- myapp/joke_posts/views.py
+++ myapp/joke_posts/views.py
@@ -24,3 +24,26 @@ def create_post():
 def joke_post(joke_post_id):
     joke_post = JokePost.query.get_or_404(joke_post_id) 
     return render_template('joke_post.html', title=joke_post.title, date=joke_post.date, post=joke_post)
+
+@joke_posts.route('/<int:joke_post_id>/update',methods=['GET','POST'])
+@login_required
+def update(joke_post_id):
+    joke_post = JokePost.query.get_or_404(joke_post_id)
+
+    if joke_post.author != current_user:
+        abort(403)
+
+    form = JokePostForm()
+
+    if form.validate_on_submit():
+        joke_post.title = form.title.data
+        joke_post.text = form.text.data
+        db.session.commit()
+        flash('Joke was Updated')
+        return redirect(url_for('joke_posts.joke_post',joke_post_id=joke_post.id))
+
+    elif request.method == 'GET':
+        form.title.data = joke_post.title
+        form.text.data = joke_post.text
+
+    return render_template('create_post.html',title='Updating',form=form)
