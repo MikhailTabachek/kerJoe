fd40be81bbf8f75334f840f2dedd30445fb0779a
Mikhail <mishanyaveselyi@gmail.com>
2022-04-12 14:53:06 -0400

    Add Joke page styling

--- myapp/joke_posts/forms.py
+++ myapp/joke_posts/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 class JokePostForm(FlaskForm):
     title = StringField('Title', validators=[DataRequired()])
-    text = TextAreaField('Text', validators=[DataRequired()])
+    text = TextAreaField('Your Joke', validators=[DataRequired()])
     submit = SubmitField('Post')
